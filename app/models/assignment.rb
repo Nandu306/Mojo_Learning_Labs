4e13b5516f783d7ac0aff32b2b11d64c015e3b64
Nandu Rajagopala <nandu.rajagopala@cantab.net>
2021-09-12 23:45:34 +0200

    more changes

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -3,6 +3,7 @@ class Assignment < ApplicationRecord
   belongs_to :taught_class
   has_many :questions, dependent: :destroy
   has_many :student_answers, through: :questions
+  has_many :class_memberships, through: :taught_class, source: :class_memberships
 
   enum status: [:completed, :in_progress, :not_started]
 
