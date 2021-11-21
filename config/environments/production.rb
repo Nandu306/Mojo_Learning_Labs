72098ca34b2e1620fe163662b19675d34325d2c0
Nandu Rajagopala <82147496+Nandu306@users.noreply.github.com>
2021-11-21 21:29:10 +0100

    changed domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,5 @@
 Rails.application.configure do
-  config.action_mailer.default_url_options = { host: "http://mojolearninglabs.com" }
+  config.action_mailer.default_url_options = { host: "http://www.mojolearninglabs.com" }
   # Settings specified here will take precedence over those in config/application.rb.
 
   ActionMailer::Base.smtp_settings = {
